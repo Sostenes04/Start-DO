efc7677aeab76685fb9f9a265e0e7b8a52e40684
Sóstenes1 <hkimaru29@gmail.com>
2023-09-01 21:05:55 +0100

    Adicionado a estrutura dos dados a se seguir

--- ToDo/Task/models.py
+++ ToDo/Task/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa (models.Model):
+
+    Estado = (
+        ('fazendo','Fazendo'),
+        ('feita','Feita')
+    )
+
+    Nome = models.CharField(max_length=100)
+    Descricao = models.TextField()
+    Status = models.CharField(
+        max_length=15,
+        choices= Estado,
+    )
+    criado_em = models.DateTimeField(auto_now_add=True)
+    actualizado_em = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.Nome
+    
